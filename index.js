904086469bdaa8a9303d0297fba6628385eba8e4
dennisobel <ogembodenis2016@gmail.com>
2022-11-08 00:24:25 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const User = require("./models/user.model");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+var morgan = require('morgan')
+
+app.use(morgan('combined'))
+app.use(cors());
+app.use(express.json());
+
+const uri =
+  "mongodb+srv://dennisobel:Beloved2022@cluster0.zlu0jub.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("DB CONNECTED"))
+  .catch(err => console.log("ERROR THROWN: ",err));
+
+app.post("/api/register", async (req, res) => {
+  try {
+    const newPassword = await bcrypt.hash(req.body.password, 10);
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: newPassword,
+    });
+    res.json({ status: "ok" });
+  } catch (err) {
+    res.json({ status: "error", error: "Duplicate email" });
+  }
+});
+
+app.post("/api/login", async (req, res) => {
+  const user = await User.findOne({
+    email: req.body.email,
+  });
+
+  if (!user) {
+    return { status: "error", error: "Invalid login" };
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+
+  if (isPasswordValid) {
+    const token = jwt.sign(
+      {
+        name: user.name,
+        email: user.email,
+      },
+      "secret123"
+    );
+
+    return res.json({ status: "ok", user: token });
+  } else {
+    return res.json({ status: "error", user: false });
+  }
+});
+
+app.listen(1337, () => {
+  console.log("Server started on 1337");
+});
+
+module.exports = app;
